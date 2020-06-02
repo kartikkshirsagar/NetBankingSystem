a6a7b671ad90dbc5ac672917af478dd4267bf12b
kartikkshirsagar <kartikpskshirsagar@gmail.com>
2020-06-02 20:51:55 +0530

    Start

--- src/loginPackage/Login.java
+++ src/loginPackage/Login.java
@@ -5,18 +5,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "login")
 public class Login extends HttpServlet {
+
+    boolean isCorrect(String user,String pass)
+    {
+        if(user.equals("root") && pass.equals("root"))
+        {
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String name=request.getParameter("uname");
         String pass=request.getParameter("pass");
         PrintWriter pw=response.getWriter();
-        if(name.equals("root") && pass.equals("root"))
+        if(isCorrect(name,pass))
         {
             pw.println("Correct");
+            HttpSession sess= request.getSession();
+            sess.setAttribute("uname",name);
             //redirect to dashboard page
         }
         else
