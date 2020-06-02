17a72b44547a76bdd349dcaedf55a119143df2b3
kartikkshirsagar <kartikpskshirsagar@gmail.com>
2020-06-02 20:08:45 +0530

    Start

--- src/loginPackage/Login.java
+++ src/loginPackage/Login.java
@@ -6,11 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "Login")
+@WebServlet(name = "login")
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String name=request.getParameter("uname");
+        String pass=request.getParameter("pass");
+        PrintWriter pw=response.getWriter();
+        if(name.equals("root") && pass.equals("root"))
+        {
+            pw.println("Correct");
+            //redirect to dashboard page
+        }
+        else
+        {
+            //redirect to auth fail
+            response.sendRedirect("AuthFail.html");
+        }
     }
-
 }
