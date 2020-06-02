cddfbee3a6d0789c27fa0aa9830efacc70bad9b9
Kartik Kshirsagar <42300545+kartikkshirsagar@users.noreply.github.com>
2020-06-02 20:02:18 +0530

    Add files via upload

---
+++ src/loginPackage/Login.java
@@ -0,0 +1,16 @@
+package loginPackage;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Login")
+public class Login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
