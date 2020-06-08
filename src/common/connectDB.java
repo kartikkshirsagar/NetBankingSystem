2628068bb2d04d469e733b905abe96ab41e0bc94
nitinosiris <nitinwartkar58@gmail.com>
2020-06-08 06:54:58 -0700

    no changes

--- src/common/connectDB.java
+++ src/common/connectDB.java
@@ -4,6 +4,7 @@ public class connectDB {
     static Connection connectToDB() throws ClassNotFoundException,SQLException {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/nbs","root","root");
+
             return con;
     }
 }
