d43537422617776a32d5472fc6b77d196adea049
Dsdroid1 <dhruvsharma2031@gmail.com>
2020-06-12 13:07:34 +0530

    Cart Database added...developement in progress..Also added Product.java

---
+++ src/MallPack/Product.java
@@ -0,0 +1,49 @@
+package MallPack;
+
+public class Product {
+    String Name;
+    int id;
+    int price;
+    String description;
+    int Quantity;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return Quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        Quantity = quantity;
+    }
+}
