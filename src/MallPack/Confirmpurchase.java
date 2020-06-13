ea0f6aaa9096c7e4948f5f5de066e2e3b371a263
karan <karanmotghare8@gmail.com>
2020-06-13 12:33:26 +0530

    Mall pack update(debited from bank)

---
+++ src/MallPack/Confirmpurchase.java
@@ -0,0 +1,48 @@
+package MallPack;
+
+import PayPackage.Account;
+import common.DButilsBank;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+public class Confirmpurchase extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession sess=request.getSession();
+        Account User=null;
+        PrintWriter out = response.getWriter();
+        int amount=Integer.parseInt(request.getParameter("amount"));
+        String Username=sess.getAttribute("uname").toString();
+        try {
+            User= DButilsBank.getAccObj(Username);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(User.getBalance() >= amount) {
+            //It is possible to withdraw
+            User.setBalance(User.getBalance() - amount);
+            try {
+                DButilsBank.UpdateBal(User);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            out.println("your order received and your current balance is ");
+            out.println(User.getBalance());
+        }
+        else
+        {
+            out.println("Insufficient balance");
+        }
+    }
+}
